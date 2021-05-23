25dda451c47c7aa82901aedc411d3ba3981e9e2d
3inchtime <3inchtime@gmail.com>
2021-05-23 20:53:28 +0800

    tracing

--- user/go.mod
+++ user/go.mod
@@ -3,10 +3,18 @@ module user
 go 1.14
 
 require (
+	github.com/HdrHistogram/hdrhistogram-go v1.1.0 // indirect
 	github.com/go-sql-driver/mysql v1.6.0
 	github.com/golang/protobuf v1.4.3
 	github.com/micro/go-micro/v2 v2.9.1
 	github.com/micro/go-plugins/config/source/consul/v2 v2.9.1
+	github.com/micro/go-plugins/wrapper/trace/opentracing/v2 v2.9.1
+	github.com/opentracing/opentracing-go v1.2.0
 	github.com/sirupsen/logrus v1.4.2
+	github.com/uber/jaeger-client-go v2.29.0+incompatible
+	github.com/uber/jaeger-lib v2.4.1+incompatible // indirect
+	go.uber.org/atomic v1.7.0 // indirect
 	golang.org/x/crypto v0.0.0-20200709230013-948cd5f35899
+	golang.org/x/lint v0.0.0-20210508222113-6edffad5e616 // indirect
+	golang.org/x/tools v0.1.1 // indirect
 )
--- user/main.go
+++ user/main.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"github.com/micro/go-micro/v2"
 	"github.com/micro/go-micro/v2/logger"
+	opentracing2 "github.com/micro/go-plugins/wrapper/trace/opentracing/v2"
+	"github.com/opentracing/opentracing-go"
+	"user/common"
 	"user/handler"
 	"user/internal/repository"
 	s "user/internal/service"
@@ -12,16 +15,26 @@ import (
 
 func main() {
 
+	// Tracing
+	t, io, err := common.NewTracer("localhost:6831", "go-shop.user")
+	if err != nil {
+		logger.Fatal(err)
+	}
+	defer io.Close()
+	opentracing.SetGlobalTracer(t)
+
 	// Create service
 	srv := micro.NewService(
 		micro.Name("user"),
 		micro.Version("latest"),
+		micro.Address("127.0.0.1:8081"),
+		micro.WrapHandler(opentracing2.NewHandlerWrapper(opentracing.GlobalTracer())),
 	)
 
 	srv.Init()
 
 	userService := s.NewUserService(repository.NewUserRepository())
-	err := pb.RegisterUserHandler(srv.Server(), &handler.User{UserService: userService})
+	err = pb.RegisterUserHandler(srv.Server(), &handler.User{UserService: userService})
 
 	if err != nil {
 		fmt.Println(err)
